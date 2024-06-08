f1bf139268a640ede90ec3445e4d44a572e5c177
Bennyjoseph07 <61040008+Bennyjoseph07@users.noreply.github.com>
2024-06-08 19:27:33 +0530

    Add files via upload

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,23 +1,39 @@
 import pandas as pd
-import re
+import re,yaml
 from docx import Document
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
 
-df = pd.read_excel('census_2011.xlsx')
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+census_data = config['census_data']
+telangana_doc = config['telangana_doc']
+
+df = pd.read_excel(census_data)
 
 
 
 
 def renaming_columnname(df):
+    """
+    Rename columns in the input dataframe to more concise and standardized names.
+
+    Parameters:
+    df (pd.DataFrame): The input dataframe with original column names.
+
+    Returns:
+    pd.DataFrame: The dataframe with renamed columns.
+    """
+   
     df.rename(columns={'State name': 'State/UT','District name':'District',
-                       'Male_Literate':'Literate_Male','Female_Literate':'Literate_Female',
-                       'Rural_Households':'Households_Rural','Urban_Households':'Households_Urban',
-                       'Age_Group_0_29':'Young_and_Adult','Age_Group_30_49':'Middle_Aged',
-                       'Age_Group_50':'Senior_Citizen','Age not stated':'Age_Not_Stated',
-                       "Households_with_Bicycle": "HouseholdsBicycle",
+                    'Male_Literate':'Literate_Male','Female_Literate':'Literate_Female',
+                    'Rural_Households':'Households_Rural','Urban_Households':'Households_Urban',
+                    'Age_Group_0_29':'Young_and_Adult','Age_Group_30_49':'Middle_Aged',
+                    'Age_Group_50':'Senior_Citizen','Age not stated':'Age_Not_Stated',
+                    "Households_with_Bicycle": "HouseholdsBicycle",
                         "Households_with_Car_Jeep_Van": "HouseholdsCarJeepVan",
                         "Households_with_Radio_Transistor": "HouseholdsRadioTransistor",
                         "Households_with_Scooter_Motorcycle_Moped": "HouseholdsScooterMotorcycleMoped",
@@ -81,51 +97,94 @@ def renaming_columnname(df):
                         "Power_Parity_Rs_330000_545000": "PowerParity330000to545000",
                         "Power_Parity_Above_Rs_545000": "PowerParityAbove545000",
                         "Total_Power_Parity": "TotalPowerParity"},inplace=True)
-
     return df
-
+    
+    
 
 
 
 def renaming_state_name(df):
+    """
+    Capitalize the first letter of each word in the 'State/UT' column 
+    and make the word 'And' lowercase in the state names.
+
+    Parameters:
+    df (pd.DataFrame): The input dataframe with a 'State/UT' column.
+
+    Returns:
+    pd.DataFrame: The dataframe with renamed state names in the 'State/UT' column.
+    """
+    
+
     df["State/UT"]= df["State/UT"].str.title()
     def lowercase_word(text, word_to_lowercase):
+        """
+        Make a specific word lowercase in a given text.
+
+        Parameters:
+        text (str): The input text.
+        word_to_lowercase (str): The word to be lowercased in the text.
+
+        Returns:
+        str: The text with the specific word lowercased.
+        """
         pattern = re.compile(r'\b' +re.escape(word_to_lowercase) + r'\b', re.IGNORECASE)
         return pattern.sub(word_to_lowercase.lower(), text)
     
     df['State/UT'] = df['State/UT'].apply(lambda x: lowercase_word(x, 'And'))
+    success_logger.info("State/UT column renamed successfully")
     return df
-
+    
 
 
 
 
 
 def new_state_formation(df,path):
-    
+    """
+    Update the 'State/UT' column in the dataframe to 'Telangana' for districts listed in the given document.
+
+    Parameters:
+    df (pd.DataFrame): The input dataframe with 'District' and 'State/UT' columns.
+    path (str): The file path to the document containing the list of districts.
+
+    Returns:
+    pd.DataFrame: The dataframe with updated 'State/UT' values for specified districts.
+    """
+
     doc = Document(path)
     doc_text =""
-      
+    
     for paragraph in doc.paragraphs:
-             doc_text += paragraph.text+','
+            doc_text += paragraph.text+','
     
     doc_text = doc_text.rstrip(',')
     dist_list= doc_text.split(',')
     
     df.loc[df['District'].isin(dist_list), 'State/UT'] = 'Telangana'
 
-
-
+    
 
 def missing_percentage(df):
+    """
+    Calculate the percentage of missing values for each column in the dataframe.
+
+    Parameters:
+    df (pd.DataFrame): The input dataframe.
+
+    Returns:
+    pd.DataFrame: A dataframe containing the missing value percentages for each column.
+    """
+    
     missing_percentage_df =pd.DataFrame()
     for column in df.columns:
         new_column_name = f'{column}'
         missing_percentage_df[new_column_name] = [round(df[column].isnull().sum() * 100 / len(df))]
         
     missing_percentage_df.reset_index(drop=True, inplace=True)
-   
+
     return missing_percentage_df
+   
 
 
 
@@ -135,7 +194,16 @@ def missing_percentage(df):
 
 
 def filling_data_population(df):
-    
+    """
+    Fill missing population data by calculating the difference between Male and Female columns.
+
+    Parameters:
+    df (pd.DataFrame): The input dataframe.
+
+    Returns:
+    pd.DataFrame: The dataframe with filled population data.
+    """
+  
     df['Male'] = df['Male'].apply(int)
     df['Female'] = df['Female'].apply(int)
     df['Population'] = df['Population'].apply(int)
@@ -148,15 +216,24 @@ def filling_data_population(df):
 
     df['Population'] = df[['Male', 'Female']].sum(axis=1)
     return df
-
+    
 
 
 
 def filling_data_literate(df):
-    df['Literate_Male'].fillna(0, inplace=True)
-    df['Literate_Female'].fillna(0, inplace=True)
-    df['Literate'].fillna(0, inplace=True)
+    """
+    Cast columns to integers for calculations (assuming data represents whole numbers)
 
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing male, female, and total literate data.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with missing values filled and total literate population recalculated.
+    """
+   
+
+       
     df['Literate_Male'] = df['Literate_Male'].apply(int)
     df['Literate_Female'] = df['Literate_Female'].apply(int)
     df['Literate'] = df['Literate'].apply(int)
@@ -169,11 +246,20 @@ def filling_data_literate(df):
 
     df['Literate'] = df[['Literate_Male', 'Literate_Female']].sum(axis=1)
     return df
+    
 
 
-def filling_data_households(df):
-    
-    
+def filling_data_households(df):   
+    """
+    Fills in missing rural or urban household data points and calculates the total households
+    based on the existing values.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing rural, urban, and total households data.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with missing values filled and total households recalculated.
+    """ 
 
     df['Households_Rural'] = df['Households_Rural'].apply(int)
     df['Households_Urban'] = df['Households_Urban'].apply(int)
@@ -187,9 +273,19 @@ def filling_data_households(df):
 
     df['Households'] = df[['Households_Rural', 'Households_Urban']].sum(axis=1)
     return df
+    
 
 
 def filling_literate_education(df): 
+    """
+    Calculates the total literate education based on the individual education levels.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing data for various education levels and total literate education.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the total literate education calculated.
+    """
 
     df['Below_Primary_Education'] = df['Below_Primary_Education'].apply(int)
     df['Primary_Education'] = df['Primary_Education'].apply(int)
@@ -208,6 +304,16 @@ def filling_literate_education(df):
 
 
 def filling_data_totaleducation(df):
+    """
+    Fills in missing literate or illiterate education data points and calculates the total
+    education based on the existing values.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing literate, illiterate, and total education data.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with missing values filled and total education recalculated.
+    """
     
     df['Literate_Education'] = df['Literate_Education'].apply(int)
     df['Illiterate_Education'] = df['Illiterate_Education'].apply(int)
@@ -224,6 +330,16 @@ def filling_data_totaleducation(df):
 
 
 def filling_data_sc(df):
+    """
+    Fills in missing male or female SC data points and calculates the total SC population
+    based on the existing values.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing male, female, and total SC data.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with missing values filled and total SC population recalculated.
+    """
     
     df['Male_SC'] = df['Male_SC'].apply(int)
     df['Female_SC'] = df['Female_SC'].apply(int)
@@ -239,6 +355,17 @@ def filling_data_sc(df):
     return df
 
 def filling_data_st(df):
+    """
+    Fills in missing male or female ST data points and calculates the total ST population
+    based on the existing values.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing male, female, and total ST data.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with missing values filled and total ST population recalculated.
+    """
+
     df['Male_ST'] = df['Male_ST'].apply(int)
     df['Female_ST'] = df['Female_ST'].apply(int)
     df['ST'] = df['ST'].apply(int)
@@ -252,7 +379,17 @@ def filling_data_st(df):
     df['ST'] = df[['Male_ST', 'Female_ST']].sum(axis=1)
     return df
 
-def filling_data_workers(df):
+def filling_data_workers(df):    
+    """
+    Fills in missing male or female worker data points and calculates the total worker population
+    based on the existing values.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing male, female, and total worker data.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with missing values filled and total worker population recalculated.
+    """
     
     df['Male_Workers'] = df['Male_Workers'].apply(int)
     df['Female_Workers'] = df['Female_Workers'].apply(int)
@@ -268,46 +405,51 @@ def filling_data_workers(df):
     return df
 
 
-##### Task 1 Renaming column names
-renaming_columnname(df)
-##### Task 2 Rename State/UT Names
-renaming_state_name(df)
-##### Task 3  New State/UT formation
-path = 'Telangana.docx'
-new_state_formation(df,path)
-######## Task 4 Find and process Missing Data and filling data
-missing_df = missing_percentage(df)
-df = df.fillna(0)
-filled_population = filling_data_population(df)
-literate_data = filling_data_literate(df)
+if __name__ == "__main__": 
 
-household_data = filling_data_households(df)
-literate_education_data =filling_literate_education(df)
-totaleducation_data = filling_data_totaleducation(df)
-sc_data= filling_data_sc(df)
-st_data = filling_data_st(df)
-workers_data = filling_data_workers(df)
+    ##### Task 1 Renaming column names
+    renaming_columnname(df)
+    ##### Task 2 Rename State/UT Names
+    renaming_state_name(df)
+    ##### Task 3  New State/UT formation
 
-missing_df_after = missing_percentage(df)
+    new_state_formation(df,telangana_doc)
+    ######## Task 4 Find and process Missing Data and filling data
+    missing_df = missing_percentage(df)
+    df = df.fillna(0)
+    filled_population = filling_data_population(df)
+    literate_data = filling_data_literate(df)
 
-appended_df = missing_df.append(missing_df_after, ignore_index=True) 
-for  i in df.columns:
-    if df[i].dtype == 'float64':
-        df[i] = df[i].apply(int)
+    household_data = filling_data_households(df)
+    literate_education_data =filling_literate_education(df)
+    totaleducation_data = filling_data_totaleducation(df)
+    sc_data= filling_data_sc(df)
+    st_data = filling_data_st(df)
+    workers_data = filling_data_workers(df)
 
-uri = "mongodb+srv://benjo:12345@cluster0.x4wlqah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    missing_df_after = missing_percentage(df)
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-db = client["census"]  # Replace with your database name
-collection = db["census_data"]  # Replace with your collection name
+    appended_df = missing_df.append(missing_df_after, ignore_index=True) 
+    for  i in df.columns:
+        if df[i].dtype == 'float64':
+            df[i] = df[i].apply(int)
 
-# Convert DataFrame to dictionary records
-records = df.to_dict(orient='records')
 
-# Insert records into MongoDB collection
-collection.insert_many(records)
-print('success')
+    uri = config['database']['mongodb']['url']
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db_name = config['database']['mongodb']['database']
+    db = client[db_name]  
+    collection_name = config['database']['mongodb']['collection']
+    collection = db[collection_name]  
+
+
+    records = df.to_dict(orient='records')
+
+
+    collection.insert_many(records)
+    print('success')
 
 
 
